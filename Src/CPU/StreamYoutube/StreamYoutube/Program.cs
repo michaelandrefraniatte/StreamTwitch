106393d6d3bc0f52bfab391c3534224709ef791c
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-30 12:32:43 +0200

    Add about

--- Src/CPU/StreamYoutube/StreamYoutube/Program.cs
+++ Src/CPU/StreamYoutube/StreamYoutube/Program.cs
@@ -9,6 +9,15 @@ namespace StreamYoutube
 {
     class Program
     {
+        static void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
         [DllImport("advapi32.dll")]
         private static extern bool LogonUser(string lpszUsername, string lpszDomain, string lpszPassword, int dwLogonType, int dwLogonProvider, out IntPtr phToken);
         [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
